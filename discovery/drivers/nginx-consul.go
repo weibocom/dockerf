8fe4f87cda8c99e972e5cd1bec43272ff5109ceb
liubin8 <liubin8@staff.weibo.com>
2015-09-07 19:46:21 +0800

    add build image

--- discovery/drivers/nginx-consul.go
+++ discovery/drivers/nginx-consul.go
@@ -3,14 +3,14 @@ package drivers
 import (
 	"errors"
 	"fmt"
-	"net/http"
-	"strings"
-	"time"
-
+	"github.com/Sirupsen/logrus"
 	consul "github.com/hashicorp/consul/api"
 	dcluster "github.com/weibocom/dockerf/cluster"
 	"github.com/weibocom/dockerf/discovery"
 	dutils "github.com/weibocom/dockerf/utils"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type ConsulOperation string
@@ -80,7 +80,7 @@ func (ngxDrv *NginxConsulRegisterDriver) buildConsulUrl(upstream string, address
 }
 
 func (ngxDrv *NginxConsulRegisterDriver) operate(host string, port int, op ConsulOperation, callable func(kv *consul.KV, address string) error) error {
-	dutils.Debug(fmt.Sprintf("%s service to nginx consul, consul address:%s, host:%s, port:%d\n", op, ngxDrv.consulAddress, host, port))
+	logrus.Debug(fmt.Sprintf("%s service to nginx consul, consul address:%s, host:%s, port:%d\n", op, ngxDrv.consulAddress, host, port))
 	address := fmt.Sprintf("%s:%d", host, port)
 	kv := ngxDrv.client.KV()
 	err := callable(kv, address)
---
+++ image/image.go
@@ -0,0 +1,110 @@
+package image
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const TMP_DIR = "/tmp/ForBuildDockerImage/"
+
+func run(dir, cmdline string, args ...string) ([]byte, error) {
+	_, err := exec.LookPath(cmdline)
+	if err != nil {
+		fmt.Printf("Not install '%s'\n", cmdline)
+		return nil, err
+	}
+
+	cmd := exec.Command(cmdline, args...)
+	cmd.Dir = dir
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+	err = cmd.Run()
+	out := buf.Bytes()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "# cd %s; %s %s\n", dir, cmd, strings.Join(args, " "))
+		os.Stderr.Write(out)
+		return nil, err
+	}
+	return out, nil
+}
+
+func gitCloneRepoIfNeed(dir, repo string) (string, error) {
+	// TODO check whether exist lastest source code from git
+	//if _, err := os.Stat(dir); err == nil { // exist
+
+	// rm -rf dir
+	_, err := run(".", "rm", "-rf", dir)
+	if err != nil {
+		fmt.Printf("Failed to rm exist source dir '%s'\n", dir)
+		return "", err
+	}
+	fmt.Printf("Removed exist source dir '%s'\n", dir)
+
+	// mkdir -p dir
+	_, err = run(".", "mkdir", "-p", dir)
+	if err != nil {
+		fmt.Printf("Failed to create source dir '%s'\n", dir)
+		return "", err
+	}
+	fmt.Printf("Create source dir '%s'\n", dir)
+
+	// git clone repo dir
+	_, err = run(".", "git", "clone", repo, dir)
+	if err != nil {
+		fmt.Printf("Failed to git clone source code '%s'\n", repo)
+		return "", err
+	}
+
+	// get commit id
+	out, err := run(dir, "git", "rev-parse", "HEAD")
+	if err != nil {
+		fmt.Printf("Failed to get git commit id\n")
+		return "", err
+	}
+	commitId := string(bytes.TrimSpace(out))
+
+	return commitId, nil
+}
+
+func buildAndPushImageIfNeed(dir, imageURL string) error {
+	// TODO check whether exist image in registry
+
+	// docker build -t imageName:tag ./
+	_, err := run(dir, "docker", "build", "-t", imageURL, "./")
+	if err != nil {
+		fmt.Printf("Failed to build docker image\n")
+		return err
+	}
+
+	// docker push image
+	_, err = run(".", "docker", "push", imageURL)
+	if err != nil {
+		fmt.Printf("Failed to push image '%s'\n", imageURL)
+		return err
+	}
+
+	return nil
+}
+
+// @return: append commitId for imageURL
+func BuildAndPushByRepo(repo, image string) (string, error) {
+	localSource := TMP_DIR + image
+	commitId, err := gitCloneRepoIfNeed(localSource, repo)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("Git clone source code '%s'\n", repo)
+
+	imageURL := image + ":" + commitId
+	err = buildAndPushImageIfNeed(localSource, imageURL)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("Buil and push to Registry OK\n")
+
+	return imageURL, nil
+}
